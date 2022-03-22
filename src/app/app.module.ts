893e54c173510ed88cb1360af649a897ee632d33
Dmrr599 <=>
2022-03-21 19:45:31 -0600

    TsCrearCita

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListCitasComponent } from './components/list-citas/list-citas.component';
 import { CrearCitaComponent } from './components/crear-cita/crear-cita.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CrearCitaComponent } from './components/crear-cita/crear-cita.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/crear-cita/crear-cita.component.ts
+++ src/app/components/crear-cita/crear-cita.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearCitaComponent implements OnInit {
 
+  nombre ='';
+  fecha ='';
+  hora ='';
+  asunto ='';
+
+  formularioIncorrecto = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  agregarCita (){
+    if (this.nombre ==''|| this.fecha =='' || this.hora =='' || this.asunto =='' ){
+  this.formularioIncorrecto = true;
+  return;
+    }
+    this.formularioIncorrecto = false;
+    //creamos objeto para enviarselos al padre
+    const CITA = {
+      nombre: this.nombre,
+      fecha: this.fecha,
+      hora: this.hora,
+      asunto: this.asunto
+    }
+    console.log (CITA);
+    this.resetCampos();
+  }
+
+  resetCampos(){
+    this.nombre = '';
+    this.fecha = '';
+    this.hora = '';
+    this.asunto = '';
+  }
+
 
 }
